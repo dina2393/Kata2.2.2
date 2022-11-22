1823a8870ec8637c8316039989c769996d30f619
Dina Zainetdinova <zntdnvdn@gmail.com>
2022-11-22 13:30:20 +0500

    second commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,11 +6,17 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
+import web.service.CarServiceImpl;
 
 @Controller
 public class CarsController {
-    @Autowired
+
     private CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
     @GetMapping(value = "/cars")
     public String showCars(@RequestParam(value = "count", required = false) String count, ModelMap model){
         model.addAttribute("cars", carService.showCarList(count));
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,45 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl  implements CarService {
+    private List<Car> carList;
+    {
+        carList = new ArrayList<>();
+        Car car1 = new Car("blue", "lada", 2012);
+        Car car2 = new Car("white", "bwv", 2015);
+        Car car3 = new Car("black", "ford", 2019);
+        Car car4 = new Car("yellow", "lada", 2020);
+        Car car5 = new Car("black", "ferrari", 2016);
+        Car car6 = new Car("orange", "volga", 1999);
+
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+        carList.add(car4);
+        carList.add(car5);
+        carList.add(car6);
+    }
+    public List<Car> showCarList(String count) {
+        List<Car> finalCarList;
+        if (count == null) {
+            finalCarList = carList;
+        }
+        else if (Integer.parseInt(count) < 5) {
+            finalCarList = carList.stream().limit(Integer.parseInt(count)).toList();
+        }
+        else {
+            finalCarList = carList;
+        }
+        return finalCarList;
+    }
+
+
+
+}
